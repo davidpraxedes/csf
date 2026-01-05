d9eb49a1fff81523ed3d4a65d4e16f431770a236
davidpraxedes <modderstore@gmail.com>
2026-01-04 22:47:50 -0300

    fix: adiciona inicializaÃ§Ã£o do analytics no main.jsx

--- carrefour-cartao/src/main.jsx
+++ carrefour-cartao/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
 import { initFacebookPixel } from './services/facebookPixel'
+import { initAnalytics } from './services/analytics'
 
 // Inicializar Facebook Pixel
 initFacebookPixel();
+
+// Inicializar Analytics
+initAnalytics();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
